4ee69a803a73531f7cb50b631d9442d61bb6de4d
John <Thebohemiangeek1@gmail.com>
2020-02-06 21:40:20 +0100

    Prettified

--- src/components/Home/FeaturedTours.js
+++ src/components/Home/FeaturedTours.js
@@ -5,10 +5,9 @@ import Title from "../Title"
 import styles from "../../css/items.module.css"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
-const getTours= graphql`
-
-query {
-    FeaturedTours: allContentfulTour(filter:{featured:{eq:true}}){
+const getTours = graphql`
+  query {
+    FeaturedTours: allContentfulTour(filter: { featured: { eq: true } }) {
       edges {
         node {
           name
@@ -26,20 +25,18 @@ query {
       }
     }
   }
-  
-
 `
 
 const FeaturedTours = () => {
-const response = useStaticQuery(getTours);
-const tours = response.FeaturedTours.edges;
+  const response = useStaticQuery(getTours)
+  const tours = response.FeaturedTours.edges
   return (
     <section className={styles.tours}>
       <Title title="featured" subtitle="tours" />
       <div className={styles.center}>
-      {tours.map(({node})=> {
-   return <Tour key={node.contentful_id} tour={node}/>
-    })}
+        {tours.map(({ node }) => {
+          return <Tour key={node.contentful_id} tour={node} />
+        })}
       </div>
 
       <AniLink fade to="/tours" className="btn-primary">
